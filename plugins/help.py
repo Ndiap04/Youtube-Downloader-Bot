ea75d81406b341ac8ec37135cc97bb3bff083c6d
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2021-07-08 23:47:33 +0700

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Saat ini hanya mendukung YouTube Single (tanpa daftar putar) Cukup kirim URL YouTube"
+    helptxt = f"🌹 **Panduan Bot**\n ➡️ **Kirim Link Video Youtube Yang Kamu Inginkan**\n ➡️ **Tentukan Resolusi Video Yang Kamu Inginkan**\n ➡️ **Bot Tidak Dapat Mendownload Playlist Dari Youtube**\n💠 **CONTOH LINK** 💠\n https://youtube.com/shorts/hsT_QdAQ64E?feature=share"
     await message.reply_text(helptxt)
