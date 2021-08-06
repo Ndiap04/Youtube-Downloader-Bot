f2cb0f64eef104e932de9383f93ef95f0bdb6aed
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2021-08-07 02:33:05 +0700

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"🌹 **Panduan Bot**\n ➡️ **Kirim Link Video Youtube Yang Kamu Inginkan**\n ➡️ **Tentukan Resolusi Video Yang Kamu Inginkan**\n ➡️ **Bot Tidak Dapat Mendownload Playlist Dari Youtube**\n💠 **CONTOH LINK** 💠\n https://youtube.com/shorts/hsT_QdAQ64E?feature=share"
+    helptxt = f"🌹 **Panduan Bot**\n ➡️ **Kirim Link Video Youtube Yang Kamu Inginkan**\n ➡️ **Tentukan Resolusi Video Yang Kamu Inginkan**\n ➡️ **Bot Tidak Dapat Mendownload Playlist Dari Youtube**\n➡️ **Untuk Memulai Download Silakan Kirim Link Pada Bot**"
     await message.reply_text(helptxt)
