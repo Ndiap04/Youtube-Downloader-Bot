80f485731daec60d80da124603247d6752291f46
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2021-08-07 02:22:21 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,8 +6,6 @@ async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("👿 Channel", url="https://t.me/fckyoupeople1")],
-        [InlineKeyboardButton(
-            "🌹 Donasi", url="https://t.me/pikyus1")]
     ])
     welcomed = f"Halo🐣 <b>{message.from_user.first_name}</b>\n 🐈 **Saya Adalah Bot Yang Dapat Mengunduh Lagu Dan Video Dari** [YouTube](https://telegra.ph/file/4341cfadb0abff6f6e60d.jpg)\n😉 **Cukup Ketik /help Untuk Mengetahui Perintahnya**"
     await message.reply_text(welcomed, reply_markup=joinButton)
