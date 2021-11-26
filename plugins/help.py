f7fee6d8863f5cc8ddae44f3091cd96c9720d9ac
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2021-11-26 20:32:43 +0700

    Delete help.py

--- plugins/help.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, Filters
-
-
-@Client.on_message(Filters.command(["help"]))
-async def start(client, message):
-    helptxt = f"🌹 **Panduan Bot**\n ➡️ **Kirim Link Video Youtube Yang Kamu Inginkan**\n ➡️ **Tentukan Resolusi Video Yang Kamu Inginkan**\n ➡️ **Bot Tidak Dapat Mendownload Playlist Dari Youtube**\n➡️ **Untuk Memulai Download Silakan Kirim Link Pada Bot**"
-    await message.reply_text(helptxt)
